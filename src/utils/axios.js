c1eca411699c10d9af97d098a8848fbec2d378b4
mohan <mohankumar.kukkala @gmail.com>
2024-01-30 01:16:07 +0530

    react changes

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://localhost:4000",
   withCredentials: true,
   timeout: 10000,
   delayed: false,
